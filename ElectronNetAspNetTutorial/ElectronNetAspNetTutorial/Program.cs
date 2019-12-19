42a47abde319cff646ea80a82001c24ff1718e41
YonedaMasashi <pillows916@gmail.com>
2019-12-19 11:48:00 +0900

    ElectronNET のプロジェクトを作成 https://qiita.com/nqdior/items/d21d67624d893225762c

---
+++ ElectronNetAspNetTutorial/ElectronNetAspNetTutorial/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ElectronNET.API; // C³
+
+namespace ElectronNetAspNetTutorial {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseElectron(args);  // C³
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
